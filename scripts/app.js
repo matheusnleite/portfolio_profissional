16f5daeaedad86d9dd6ad006af85da0a8d4500e4
Matheus <matheusn06leite@gmail.com>
2025-02-20 17:55:13 -0300

    criando a seção de hard-skills com animação de barras de progresso

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+// Carrossel
+
 let indiceAtual = 0;
 
 function mostrarSection(indice) {
@@ -23,3 +25,47 @@ function nextSection() {
 function prevSection() {
   mostrarSection(indiceAtual - 1);
 }
+
+//Barra de progresso da seção Skills
+
+function animarBarraDeProgresso() {
+  const barraDeProgresso = document.querySelectorAll(".progresso"); //seleciona todas as barras de progresso
+
+  barraDeProgresso.forEach((barra) => {
+    //barra = representa cada barra individualmente
+
+    //Pegando o valor da porcentagem da barra pelo atributo data-progresso
+    const valor = barra.getAttribute("data-progresso");
+
+    barra.style.width = valor + "%"; //definindo a largura da barra de progresso
+  });
+}
+
+//funcao que reseta a barra de progresso
+function resetarBarraDeProgresso() {
+  const barraDeProgresso = document.querySelectorAll(".progresso");
+
+  barraDeProgresso.forEach((barra) => {
+    barra.style.width = "0%";
+  });
+}
+
+//Função para animar a barra de progresso quando o usuário rolar a página
+
+const observador = new IntersectionObserver((entries) => {
+  //lista de elementos visiveis na tela
+  entries.forEach((entry) => {
+    //se o elemento estiver visivel
+    if (entry.isIntersecting) {
+      animarBarraDeProgresso();
+    }
+    //se o elemento nao estiver visivel
+    else {
+      resetarBarraDeProgresso();
+    }
+  });
+});
+
+//Observando a div skills__conteudo onde estão as habilidades
+
+observador.observe(document.querySelector(".skills__conteudo"));
